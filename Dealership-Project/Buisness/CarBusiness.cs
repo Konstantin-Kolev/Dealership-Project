467fb54c2c35e04e1704de437de8ac8a05bdb3aa
SogoProds <sogoprods@gmail.com>
2019-03-20 11:33:53 +0200

    Created classes in the Business folder, without adding any logic to them. Made adjustments to the name of the Business folder. Deleted a warning line of code in the CarDealershipContext class.

---
+++ Dealership-Project/Buisness/CarBusiness.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Models;
+
+namespace Buisness
+{
+    class CarBusiness
+    {
+        //NOT DONE//
+        private CarDealershipContext carContext;
+    }
+}
---
+++ Dealership-Project/Buisness/CustomerBusiness.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Models;
+
+namespace Buisness
+{
+    class CustomerBusiness
+    {
+        //NOT DONE//
+        private CarDealershipContext customerContext;
+    }
+}
