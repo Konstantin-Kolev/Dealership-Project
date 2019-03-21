d350441f542228c1ef840e9f52065ed2ae8ae670
veli_vasilev_ <veli_vasilev_@abv.bg>
2019-03-21 19:11:30 +0200

    Added CRUD to CarBusiness class

--- Dealership-Project/Buisness/CarBusiness.cs
+++ Dealership-Project/Buisness/CarBusiness.cs
@@ -9,7 +9,57 @@ namespace Buisness
 {
     class CarBusiness
     {
-        //NOT DONE//
         private CarDealershipContext carContext;
+
+        public List<Car> GetAllCars()
+        {
+            using (carContext = new CarDealershipContext())
+            {
+                return carContext.Cars.ToList();
+            }
+        }
+
+        public Car GetCar(int id)
+        {
+            using (carContext = new CarDealershipContext())
+            {
+                return carContext.Cars.Find(id);
+            }
+        }
+
+        public void Add(Car car)
+        {
+            using (carContext = new CarDealershipContext())
+            {
+                carContext.Cars.Add(car);
+                carContext.SaveChanges();
+            }              
+        }
+
+        public void Update(Car car)
+        {
+            using (carContext = new CarDealershipContext())
+            {
+                var item = carContext.Cars.Find(car.Id);
+                if (item != null)
+                {
+                    carContext.Entry(item).CurrentValues.SetValues(car);
+                    carContext.SaveChanges();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (carContext = new CarDealershipContext())
+            {
+                var car = carContext.Cars.Find(id);
+                if (car != null)
+                {
+                    carContext.Cars.Remove(car);
+                    carContext.SaveChanges();
+                }
+            }
+        }
     }
 }
