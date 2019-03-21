be69970e75ac170b501c7ed3d6e92e3760e9ce1d
veli_vasilev_ <veli_vasilev_@abv.bg>
2019-03-21 18:09:50 +0200

    Added CRUD to EngineBusiness class

--- Dealership-Project/Buisness/EngineBusiness.cs
+++ Dealership-Project/Buisness/EngineBusiness.cs
@@ -9,7 +9,57 @@ namespace Buisness
 {
     class EngineBusiness
     {
-        //NOT DONE//
         private CarDealershipContext engineContext;
+
+        public List<Engine> GetAllEngines()
+        {
+            using (engineContext = new CarDealershipContext())
+            {
+                return engineContext.Engines.ToList();
+            }
+        }
+
+        public Engine GetEngine(int id)
+        {
+            using (engineContext = new CarDealershipContext())
+            {
+                return engineContext.Engines.Find(id);
+            }
+        }
+
+        public void Add(Engine engine)
+        {
+            using (engineContext = new CarDealershipContext())
+            {
+                engineContext.Engines.Add(engine);
+                engineContext.SaveChanges();
+
+            }
+        }
+
+        public void Update(Engine engine)
+        {
+            using (engineContext = new CarDealershipContext())
+            {
+                var item = engineContext.Engines.Find(engine.Id);
+                if (item != null)
+                {
+                    engineContext.Entry(item).CurrentValues.SetValues(engine);
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (engineContext = new CarDealershipContext())
+            {
+                var engine = engineContext.Engines.Find(id);
+                if (engine != null)
+                {
+                    engineContext.Engines.Remove(engine);
+                    engineContext.SaveChanges();
+                }
+            }
+        }
     }
 }
