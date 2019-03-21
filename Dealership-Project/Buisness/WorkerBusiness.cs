ea26bce1c9365b8e5b2f0dd7abbd8fe527275698
veli_vasilev_ <veli_vasilev_@abv.bg>
2019-03-21 17:57:52 +0200

    Added CRUD to WorkerBusiness clas

--- Dealership-Project/Buisness/WorkerBusiness.cs
+++ Dealership-Project/Buisness/WorkerBusiness.cs
@@ -9,7 +9,57 @@ namespace Buisness
 {
     class WorkerBusiness
     {
-        //NOT DONE//
         private CarDealershipContext workerContext;
+
+        public List<Worker> GetAllWorkers()
+        {
+            using (workerContext = new CarDealershipContext())
+            {
+                return workerContext.Workers.ToList();
+            }
+        }
+
+        public Worker GetWorker(int id)
+        {
+            using (workerContext = new CarDealershipContext())
+            {
+                return workerContext.Workers.Find(id);
+            }
+        }
+
+        public void Add(Worker worker)
+        {
+            using (workerContext = new CarDealershipContext())
+            {
+                workerContext.Workers.Add(worker);
+                workerContext.SaveChanges();
+            }
+        }
+
+        public void Update(Worker worker)
+        {
+            using (workerContext = new CarDealershipContext())
+            {
+                var item = workerContext.Workers.Find(worker.Id);
+                if (item != null)
+                {
+                    workerContext.Entry(item).CurrentValues.SetValues(worker);
+                    workerContext.SaveChanges();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (workerContext = new CarDealershipContext())
+            {
+                var worker = workerContext.Workers.Find(id);
+                if (worker != null)
+                {
+                    workerContext.Workers.Remove(worker);
+                    workerContext.SaveChanges();
+                }
+            }
+        }
     }
 }
