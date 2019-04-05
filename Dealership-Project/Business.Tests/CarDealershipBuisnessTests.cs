f9acd6d7b679968bb9c0da01eb1fe1c3f8347c32
Konstantin KOlev <konstantin.kolevv@gmail.com>
2019-04-05 15:11:24 +0300

    Added NUnit tests for CarDealership class. Some are not working!

--- Dealership-Project/Business.Tests/CarDealershipBuisnessTests.cs
+++ Dealership-Project/Business.Tests/CarDealershipBuisnessTests.cs
@@ -16,7 +16,7 @@ namespace Business.Tests
     public class CarDealershipBuisnessTests
     {
         [Test]
-        public void CreateCarDealership()
+        public void AddCarDealership()
         {
             var mockSet = new Mock<DbSet<CarDealership>>();
 
@@ -28,6 +28,136 @@ namespace Business.Tests
 
             mockSet.Verify(m => m.Add(It.IsAny<CarDealership>()), Times.Once());
             mockContext.Verify(m => m.SaveChanges(), Times.Once());
+        }
+
+        [Test]
+        public void GetAllDealerships()
+        {
+            var data = new List<CarDealership>
+            {
+                new CarDealership{ Name="CarDealership1"},
+                new CarDealership{ Name="CarDealership2"},
+                new CarDealership{ Name="CarDealership3"},
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<CarDealership>>();
+            mockSet.As<IQueryable<CarDealership>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<CarDealership>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<CarDealership>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<CarDealership>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            var mockContext = new Mock<CarDealershipContext>();
+            mockContext.Setup(m => m.CarDealerships).Returns(mockSet.Object);
+
+            var service = new CarDealershipBusiness(mockContext.Object);
+            var carDealershipsFound = service.GetAllCarDealerships();
+
+            Assert.AreEqual(3, carDealershipsFound.Count());
+            Assert.AreEqual("CarDealership1", carDealershipsFound[0].Name);
+            Assert.AreEqual("CarDealership2", carDealershipsFound[1].Name);
+            Assert.AreEqual("CarDealership3", carDealershipsFound[2].Name);
+        }
+
+        [Test]
+        public void GetCarDealershipById()
+        {
+            var data = new List<CarDealership>
+            {
+                new CarDealership{ Name="CarDealership1",Id=1},
+                new CarDealership{ Name="CarDealership2",Id=2},
+                new CarDealership{ Name="CarDealership3",Id=3},
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<CarDealership>>();
+            mockSet.As<IQueryable<CarDealership>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<CarDealership>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<CarDealership>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<CarDealership>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            var mockContext = new Mock<CarDealershipContext>();
+            mockContext.Setup(m => m.CarDealerships).Returns(mockSet.Object);
+
+            var service = new CarDealershipBusiness(mockContext.Object);
+            var carDealershipFound = service.GetCarDealershipById(1);
+
+            Assert.AreEqual("CarDealership1", carDealershipFound.Name);
+        }
+
+        [Test]
+        public void GetCarDealershipByName()
+        {
+            var data = new List<CarDealership>
+            {
+                new CarDealership{ Name="CarDealership1"},
+                new CarDealership{ Name="CarDealership2"},
+                new CarDealership{ Name="CarDealership3"},
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<CarDealership>>();
+            mockSet.As<IQueryable<CarDealership>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<CarDealership>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<CarDealership>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<CarDealership>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            var mockContext = new Mock<CarDealershipContext>();
+            mockContext.Setup(m => m.CarDealerships).Returns(mockSet.Object);
+
+            var service = new CarDealershipBusiness(mockContext.Object);
+            var dealershipFound = service.GetCarDealershipByName("CarDealership1");
+
+            Assert.AreEqual("CarDealership1", dealershipFound.Name);
+        }
+
+        [Test]
+        public void GetcarDealershipByTown()
+        {
+            var data = new List<CarDealership>
+            {
+                new CarDealership{ Name="CarDealership1",Town=new Town{ Name="Town1"} },
+                new CarDealership{ Name="CarDealership2",Town=new Town{ Name="Town2"} },
+                new CarDealership{ Name="CarDealership3",Town=new Town{ Name="Town1"} },
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<CarDealership>>();
+            mockSet.As<IQueryable<CarDealership>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<CarDealership>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<CarDealership>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<CarDealership>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            var mockContext = new Mock<CarDealershipContext>();
+            mockContext.Setup(m => m.CarDealerships).Returns(mockSet.Object);
+
+            var service = new CarDealershipBusiness(mockContext.Object);
+            var dealershipsFound = service.GetDealershipsByTown("Town1");
+
+            Assert.AreEqual(2, dealershipsFound.Count());
+            Assert.AreEqual("CarDealership1", dealershipsFound[0].Name);
+            Assert.AreEqual("CarDealership3", dealershipsFound[1].Name);
+        }
+
+        [Test]
+        public void GetTownName()
+        {
+            var data = new List<CarDealership>
+            {
+                new CarDealership{ Name="CarDealership1"},
+                new CarDealership{ Name="CarDealership2",Town=new Town{ Id=6,Name="Town1"} },
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<CarDealership>>();
+            mockSet.As<IQueryable<CarDealership>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<CarDealership>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<CarDealership>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<CarDealership>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            var mockContext = new Mock<CarDealershipContext>();
+            mockContext.Setup(m => m.CarDealerships).Returns(mockSet.Object);
+
+            var service = new CarDealershipBusiness(mockContext.Object);
+            var list = data.ToList();
+            var townNameFound = service.GetTownName(list[1].Town.Id);
+
+            Assert.AreEqual("Town1", townNameFound);
         }
     }
 }
